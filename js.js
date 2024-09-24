714bfd6877f2f44fd906972f29d7dd0a31146932
LijaTureca <124587378+LijaTureca@users.noreply.github.com>
2024-09-24 11:48:12 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    fetch('sidebar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('sidebar').innerHTML = data;
+        })
+    
+        fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        })
+
+        fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        })
+    
+};
